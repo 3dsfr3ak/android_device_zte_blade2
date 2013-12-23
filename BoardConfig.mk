a0df14d3dfd96b119cf04d5cf3a14e554df5089f
3dsfr3ak <matthias-schmitt98@gmx.de>
2013-12-23 19:39:06 +0100

    Fix kernel compilation
    Change-Id: If62ed236e851957a66aa412f5ff7dbf1200ec6d3

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -35,15 +35,17 @@ TARGET_NO_RADIOIMAGE := true
 TARGET_NO_BOOTLOADER := true
 
 # Recovery
-TARGET_PREBUILT_RECOVERY_KERNEL := device/zte/blade2/recovery_kernel
 BOARD_CUSTOM_GRAPHICS := ../../../device/zte/blade2/recovery/graphics.c
 BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/zte/blade2/recovery/recovery_ui.c
 TARGET_RECOVERY_INITRC := device/zte/blade2/recovery/recovery.rc
 
 # Kernel
-TARGET_KERNEL_SOURCE := kernel/zte/zte-kernel-msm7x27
-TARGET_KERNEL_CONFIG := cyanogen_blade2_p736v_defconfig
-TARGET_PREBUILT_KERNEL := device/zte/blade2/prebuilt/kernel
+TARGET_KERNEL_SOURCE := kernel/zte/msm7x27
+ifdef P736E
+TARGET_KERNEL_CONFIG := blade2_jb_p736e_defconfig
+else
+TARGET_KERNEL_CONFIG := blade2_jb_p736v_defconfig
+endif
 BOARD_KERNEL_BASE := 0x02600000
 BOARD_KERNEL_CMDLINE := androidboot.hardware=blade2 console=null
 
